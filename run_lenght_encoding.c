1ce02d009c972e14eaf248fc9af3a990f5ea50af
EliaDassio <71812349+EliaDassio@users.noreply.github.com>
2020-10-15 11:50:29 +0200

    Update run_lenght_encoding.c

--- run_lenght_encoding.c
+++ run_lenght_encoding.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (){
     
     char word[100]; //creation of the array that will ontain the word to encode or decode
-    char ed;    //creation of a variabol that will determing if the uer chouse to decode or encode
+    int ed;    //creation of a variabol that will determing if the uer chouse to decode or encode
     int n;
     int num, sum;
     
-     printf("do you wanto to decode [d] or encode [e] ? "); //ask the user if they want to decode or encode
-     scanf("%s", &ed);
+     printf("do you wanto to decode [0] or encode [1] ? "); //ask the user if they want to decode or encode
+     scanf("%d", &ed);
 
-     if (ed != 'e' && ed != 'd'){ // if the answer is not accettable I'll re ask the question untill it's right
-         while (ed != 'e' && ed != 'd'){
-             printf ("\nplease insert a valid answer [e] [d]: ");
-             scanf("%s", &ed);
-         }
+     if (ed != 1 && ed != 0){ // if the answer is not accettable I'll re ask the question untill it's right
+         do{
+             printf ("\nplease insert a valid answer [1] [0]: ");
+             scanf("%c", &ed);
+         }while (ed != 1 && ed != 0);
      }
 
-     printf("\ninput lenth of string: "); // ask the user to input the lenght of the string so that they can input the string with a for loop
-     scanf("%d", &n);
+     printf("\ninput string: ");
+     scanf("%s", word);
 
-     for(int i = 0; i < n; i++){
-         scanf("%s", word[i]);
-     }
+
 
      //--------------------------------DECODING--------------------------------
 
-     if (ed == 'd'){
-         printf("\nprint the decoding\n");
-         for (int i = 0; i < n; i++){               // a loop for scan every cell of the array
-             if (word[i] >= 48 && word[i] <= 57){   // chek if the cell contain a number and convert thet number from achii to int
-                num = word[i] - 48;
-                 for (int j = 0; j <= num; j++){    //print the next cell num times
-                     printf("%s", word[i+1]);
+     if (ed == 0){
+         printf("\nprint the decoding\n\n");
+         for (int i = 0; i < strlen(word); i++){               // a loop for scan every cell of the array
+             if (word[i] >= '0' && word[i] <= '9'){   // chek if the cell contain a number and convert thet number from achii to int
+                num = word[i] - '0';
+                 for (int j = 0; j < num; j++){    //print the next cell num times
+                     printf("%c", word[i+1]);
                  }
                  i++;                               //jump to the next cell
              }
@@ -44,9 +43,23 @@ int main (){
 
      //--------------------------------ENCODING--------------------------------
 
-     if (ed == 'e'){
-
+     if (ed == 1){
+         printf("\nprint the encoding\n\n");
+         
+         num = 1;
+         
+         for (int i = 0; i < strlen(word); i++){
+             if (word[i] == word[i+1]){
+                 num++;
+             }
+             else{
+                 printf("%d%c", num, word[i]);
+                 num = 1;
+             }
+         }
      }
     
-     return 0;
+    printf("\n");
+    
+    return 0;
 } 
